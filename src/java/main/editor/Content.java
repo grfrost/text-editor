809c4940ec4be57b0c446b6a37daacc35b214476
grfrost <frost.gary@gmail.com>
2025-06-17 17:44:55 +0100

    Debug off by one

--- src/java/main/editor/Content.java
+++ src/java/main/editor/Content.java
@@ -10,8 +10,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Content implements RowColBounds<Content> {
-
-
     public static class Line implements ColBounds<Line> {
         Content content;
         private String text;
@@ -35,13 +33,13 @@ public class Content implements RowColBounds<Content> {
             return 0;
         }
 
-        public static Line of(Content content,String s) {
-            return new Line(content,s);
+        public static Line of(Content content, String s) {
+            return new Line(content, s);
         }
 
         public void deleteChar(Col<?> col) {
             if (containsCol(col)) {
-                 replace(new StringBuilder(text).deleteCharAt(col.col()).toString());
+                replace(new StringBuilder(text).deleteCharAt(col.col()).toString());
             }
         }
 
@@ -50,20 +48,46 @@ public class Content implements RowColBounds<Content> {
         }
 
         public void replace(String replacement) {
-            text=replacement;
+            text = replacement;
         }
+    }
+
+    private Line lineAt(int row) {
+        return containsRow(row) ? c.get(row) : null;
     }
 
     final private List<Line> c;
 
     public Content(Stream<String> lines) {
         this.c = lines
-                .map(s->new Content.Line(this, s))
+                .map(s -> new Content.Line(this, s))
                 .collect((Collectors.toCollection(ArrayList::new)));
     }
 
     public Content(String text) {
         this(Stream.of(text.split("\n")));
+    }
+
+
+    @Override
+    public int maxRow() {
+        return c.size();
+    }
+
+    @Override
+    public int maxCol() {
+        throw new IllegalStateException("Does not make sense in this method");
+        // return c.stream().map(Line::maxCol).max(Integer::compareTo).get();
+    }
+
+    @Override
+    public int minRow() {
+        return 0;
+    }
+
+    @Override
+    public int minCol() {
+        return 0;
     }
 
     public static Content of(Path path) {
@@ -78,33 +102,11 @@ public class Content implements RowColBounds<Content> {
         }
     }
 
-    @Override
-    public int maxRow() {
-        return c.size();
-    }
 
-    @Override
-    public int maxCol() {
-        throw new IllegalStateException("Does not make sense in this method");
-       // return c.stream().map(Line::maxCol).max(Integer::compareTo).get();
-    }
-
-    @Override
-    public int minRow() {
-        return 0;
-    }
-
-    @Override
-    public int minCol() {
-        return 0;
-    }
-
-    Line lineAt(int row) {
-        return containsRow(row) ? c.get(row) : null;
-    }
     public Line line(Row<?> row) {
         return containsRow(row) ? c.get(row.row()) : null;
     }
+
     public Line lineAbove(Row<?> row) {
         return lineAt(row.row() - 1);
     }
@@ -132,83 +134,21 @@ public class Content implements RowColBounds<Content> {
         }
     }
 
-    Line set(int row, Line line) {
-        this.c.set(row, line);
-        return line;
-    }
-
-    void deleteChar(Cursor cursor) {
-        if (line(cursor) instanceof Line line) {
-            line.deleteChar(cursor);
-        }
-    }
-
-    private Content.Line createLine(String s){
+    Content.Line createLine(String s) {
         return new Content.Line(this, s);
     }
-    private Content.Line createEmptyLine(){
-        return createLine("");
-    }
 
-    Line replace(Row<?> row, Line replacement) {
-        if (line(row) instanceof Line line) {
-            set(row.row(), replacement);
-        }
-        return replacement;
-    }
-    void appendAtLine(Row<?> row, Content.Line append) {
-        replace(row, createLine(line(row).text + append));
+    Content.Line createEmptyLine() {
+        return createLine("");
     }
 
     void insertRowAt(int row, Content.Line rowContent) {
         if (containsRow(row)) {
-            set(row, rowContent);
+            this.c.set(row, rowContent);
         }
     }
-
-    void insertCharAndMoveCursorRight(Cursor cursor, Key k) {
-        if (cursor.col() == maxRow()) {
-            insertRowAt(maxRow(), createEmptyLine());
-        }
-        if (line(cursor) instanceof Line line && line.containsCol(cursor)) {
-            line.insertChar(cursor, (char)k.v());
-            cursor.right();
-        }
-    }
-
-    void insertNewLineAndMoveCursorToNextLine(Cursor cursor) {
-        if (line(cursor) instanceof Line line) {
-            if (cursor.onFirstCol(line)){
-                insertRowAt(cursor.row(), createEmptyLine());
-                // Add an empty line above
-            }else if (cursor.onLastCol(line)){
-                insertRowAt(cursor.row()+1, createEmptyLine());
-                // Add an empty line below
-            }else{
-                String text = line(cursor).text();
-                line.replace(text.substring(0, cursor.col()));
-                Line right = createLine(text.substring(cursor.col()));
-                insertRowAt(cursor.row()+1, right);
-            }
-            cursor.down(1).col(0);
-        }
-    }
-    public void deleteCharBeforeCursorAndAdjustCursor(Cursor cursor) {
-        if (line(cursor) instanceof Content.Line line ) {
-            if (cursor.onFirstCol(line)) {
-                if (!cursor.onFirstRow(this)){
-                    var lineAbove = lineAbove(cursor);
-                    var text = lineAbove.text();
-                    text = text.substring(0, text.length() - 1);
-                    text = text + line.text();
-                    lineAbove.replace(text);
-                    deleteLine(cursor);
-                    cursor.up(1).col(text.length());
-                }
-            }else {
-                cursor.left();
-                line.deleteChar(cursor);
-            }
-        }
+    void insertEmptyLine(Row<?> row) {
+        insertRowAt(row.row(), createEmptyLine());
     }
 }
+
--- src/java/main/editor/Editor.java
+++ src/java/main/editor/Editor.java
@@ -1,14 +1,21 @@
 package editor;
 
 import editor.terminal.ANISTerminal;
-import editor.terminal.WindowSize;
 import editor.terminal.JlineTerminal;
+import editor.terminal.WindowSize;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class Editor {
+    record Status(String message) {
+        public static Status of(String messae) {
+            return new Status(messae);
+        }
+    }
+
     static int leftmargin = 12; // Line # + line len
+    Status status = Status.of("OK");
 
     private void refreshScreen() {
         viewport = viewport.track(cursor);
@@ -21,9 +28,11 @@ public class Editor {
             }
         }
         terminal.inv(_ ->
-                terminal.fill(windowSize.maxCol(), "Screen: " + windowSize.maxDims()
+                terminal.fill(windowSize.maxCol(), windowSize.maxDims()
                         + " Lines:" + content.maxRow()
-                        + " Viewport " + viewport.dims()
+                        + " View " + viewport.dims()
+                        + " Curs " + cursor.row() + ":" + cursor.col()
+                        + " Stat " + status.message()
                 )
         );
         terminal.rowCol(cursor.row() - viewport.minRow(), cursor.col() - viewport.minCol() + leftmargin);
@@ -43,23 +52,63 @@ public class Editor {
         } else if (content.line(cursor) instanceof Content.Line line) {
             // These all require us to be in the content
             if (key == Key.NL) {
-                content.insertNewLineAndMoveCursorToNextLine(cursor);
+                if (cursor.onFirstCol(line)) {
+                    content.insertEmptyLine(cursor);
+                    cursor.down(1);
+                    status= Status.of("INS NL COL1");
+                    // Add an empty line above
+                } else if (cursor.onLastCol(line)) {
+                    cursor.down(1);
+                    content.insertEmptyLine(cursor);
+                    status= Status.of("INS NL LINE END");
+                    // Add an empty line below
+                } else {
+                    String text = line.text();
+                    line.replace(text.substring(0, cursor.col()));
+                    Content.Line right = content.createLine(text.substring(cursor.col()));
+                    content.insertRowAt(cursor.row() + 1, right);
+                    status = Status.of("INS NL mid line");
+                    cursor.down(1);
+                }
+                cursor.col(0);
             } else if (key == Key.BACKSPACE || key == Key.CtrlH || key == Key.DEL) {
-                content.deleteCharBeforeCursorAndAdjustCursor(cursor);
+                if (cursor.onFirstCol(line)) {
+                    if (!cursor.onFirstRow(content)) {
+                        var lineAbove = content.lineAbove(cursor);
+                        var text = lineAbove.text();
+                        text = text.substring(0, text.length() - 1);
+                        text = text + line.text();
+                        lineAbove.replace(text);
+                        content.deleteLine(cursor);
+                        cursor.up(1).col(text.length());
+                        status = Status.of("DEL CH and joined lines");
+                    }else{
+                        status = Status.of("DEL CH ignored");
+                    }
+                } else {
+                    cursor.left();
+                    line.deleteChar(cursor);
+                    status = Status.of("DEL CH in line");
+                }
+
             } else if (key == Key.ARROW_UP && !cursor.onFirstRow(content)) {
                 cursor.up(1);
                 if (content.line(cursor) instanceof Content.Line l && cursor.isRightOf(l)) {
                     cursor.col(l.maxCol());
                 }
+                status = Status.of("ARROW_UP");
             } else if (key == Key.ARROW_DOWN && !cursor.onLastRow(content)) {
                 cursor.down(1);
                 if (content.line(cursor) instanceof Content.Line l && cursor.isRightOf(l)) {
                     cursor.col(l.maxCol());
                 }
+                status = Status.of("ARROW_DOWN");
             } else if (key == Key.ARROW_LEFT && !cursor.onFirstCol(line)) {
                 cursor.left();
+                status = Status.of("<--");
             } else if (key == Key.ARROW_RIGHT && !cursor.onLastCol(line)) {
                 cursor.right();
+                status = Status.of("-->");
             } else if (key == Key.PAGE_UP) {
                 for (int i = 0; !cursor.onFirstRow(content) && i < viewport.height(); i++) {
                     cursor.up(1);
@@ -78,9 +127,15 @@ public class Editor {
                 cursor.col(0);
             } else if (key == Key.END) {
                 cursor.col(line.maxCol());
+            } else if (line.containsCol(cursor)) {
+                line.insertChar(cursor, (char) key.v());
+                cursor.right();
+                status = Status.of("inserted " + key.v());
+            } else {
+                status = Status.of("UNHANDLED 1 " + key.v());
             }
         } else {
-            content.insertCharAndMoveCursorRight(cursor, key);
+            status = Status.of("UNHANDLED 2" + key.v());
         }
     }
 
